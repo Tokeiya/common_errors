b165e5cea2d94ed560a07163b44ddae6196c2fe7
tokeiya3 <net_seed@hotmail.com>
2025-12-04 20:49:35 +0900

    Add `description` method and `From<String>` implementation for `Error` struct.

--- common_errors/src/invalid_operation.rs
+++ common_errors/src/invalid_operation.rs
@@ -4,3 +4,15 @@ use thiserror::Error as ThisError;
 #[error("invalid operation:{0}")]
 pub struct Error(String);
 
+
+impl Error {
+	pub fn description(&self)->&str{
+		&self.0
+	}
+}
+
+impl From<String> for Error {
+	fn from(value: String) -> Self {
+		Self(value)
+	}
+}
