76ba15c61fe27c424c4c80042fec477c52f3b00e
tokeiya3@Home <net_seed@hotmai.com>
2026-01-15 22:10:16 +0900

    bump common_errors crate to v0.4.1 and rename IndexOutOfRangeError to Error

--- common_errors/src/index_out_of_range.rs
+++ common_errors/src/index_out_of_range.rs
@@ -3,16 +3,16 @@ use thiserror::Error as ThisError;
 
 #[derive(Debug, ThisError)]
 #[error("index out of range:{0}")]
-pub struct  IndexOutOfRangeError(String);
+pub struct  Error(String);
 
 
-impl From<String> for IndexOutOfRangeError {
+impl From<String> for Error {
 	fn from(value: String) -> Self {
 		Self(value)
 	}
 }
 
-impl From<&str> for IndexOutOfRangeError {
+impl From<&str> for Error {
 	fn from(value: &str) -> Self {
 		Self(value.to_string())
 	}
